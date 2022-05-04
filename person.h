39b4ce6888a064b2bf753871727d19288b552ac7
lesy-act <lesykma2000@gmail.com>
2022-05-05 00:25:42 +0700

    add age member

--- person.h
+++ person.h
@@ -8,6 +8,7 @@ protected:
     std::string name;
     std::string address;
     std::string id;
+    int age;
 public:
     Person();
     Person(std::string name,std::string address,std::string id);
@@ -16,9 +17,11 @@ public:
     void set_name(std::string name);
     void set_address(std::string address);
     void set_id(std::string id);
+    void set_age(int age);
     std::string get_name();
     std::string get_address();
     std::string get_id();
+    int get_age();
 
 };
 
